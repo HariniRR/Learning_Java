fcb8bb24ea4db82e96e71d8634a43da70b7aad37
Harini R R <141791926+HariniRR@users.noreply.github.com>
2025-05-24 14:22:23 +0530

    Update ExceptionHandling3.java

--- basicConceptsOfJava/ExceptionHandling3.java
+++ basicConceptsOfJava/ExceptionHandling3.java
@@ -1,11 +1,29 @@
 package basicConceptsOfJava;
 
 public class ExceptionHandling3 {
-
+void validity(int age) throws IllegalArgumentException{
+	try {
+		if (age<0)
+			throw new IllegalArgumentException("Age can not be negaative");
+		else
+			System.out.println("Access Granted");
+	}
+	catch (Exception e) {
+		System.out.println("Exception caught: " + e.getMessage());
+	}
+	
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ExceptionHandling3 obj = new ExceptionHandling3();
+		int age=-5;
+		int age1=20;
+		obj.validity(age1);
+		obj.validity(age);
 	}
 
 }
-//throw(s)
+/*Output
+Access Granted
+Exception caught: Age can not be negaative
+*/
