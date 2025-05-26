e9fdc681b0518798944b4ff8af235c69fca92bb9
Harini R R <141791926+HariniRR@users.noreply.github.com>
2025-05-26 15:14:37 +0530

    Update Cycle.java

--- basicConceptsOfJava/myInterface/Cycle.java
+++ basicConceptsOfJava/myInterface/Cycle.java
@@ -1,6 +1,7 @@
 package myInterface;
 
 public interface Cycle {
+	//	public static final int applybreak = 0; 
 	void setHeight(int newHeight);
 	void speedup(int accelerator);
 	void speeddown(int breaks);
