0cd9171fa7d5fcda04130f8072657b2c1f2ffe4a
Harini R R <141791926+HariniRR@users.noreply.github.com>
2025-05-26 13:33:59 +0530

    Update OopAbstraction.java

--- basicConceptsOfJava/OopAbstraction.java
+++ basicConceptsOfJava/OopAbstraction.java
@@ -9,7 +9,7 @@ abstract class Shape{
 	System.out.println("This is shape class");
 	}
 }
-class SquareArea extends Shape{
+class Square extends Shape{
 	int length;
 	int area;
 	void setLength(int length) {
@@ -19,8 +19,54 @@ class SquareArea extends Shape{
 	int getArea() {
 		return length * length;
 	}
+	@Override
+	int getVolume() {
+		return length * length * length;
+	}
 }
-class SquareVolume extends Shape{
+public class OopAbstraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the length  of the circle, a: ");
+		int a=sc.nextInt();
+		Square sq=new Square();
+		sq.setLength(a);
+		System.out.println("Area of the Square: " + sq.getArea());
+		System.out.println("Volume of the Square: " + sq.getVolume());
+	}
+}
+/*Output
+Enter the length  of the circle, a: 
+5
+Area of the Square: 25
+Volume of the Square: 125
+*/
+
+
+
+
+
+/*abstract class Shape{
+	abstract int getArea();
+	abstract int getVolume();
+	void display() {
+	System.out.println("This is shape class");
+	}
+}
+class SquareArea extends Shape{  		//shows error as getVolume is not defined here
+	int length;
+	int area;
+	void setLength(int length) {
+		this.length=length;
+	}
+	@Override
+	int getArea() {
+		return length * length; 
+	}
+}
+class SquareVolume extends Shape{		//shows error as getArea is not defined here
 	int length;
 	int volume;
 	void setLength(int length) {
@@ -46,9 +92,4 @@ public class OopAbstraction {
 		System.out.println("Volume of the Square: " + sv.getVolume());
 	}
 }
-/*Output
-Enter the length  of the circle, a: 
-5
-Area of the Square: 25
-Volume of the Square: 125
 */
