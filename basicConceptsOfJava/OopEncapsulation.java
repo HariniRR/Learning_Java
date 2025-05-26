7872f3474338f5584e40bda66f70aab390985af4
Harini R R <141791926+HariniRR@users.noreply.github.com>
2025-05-26 12:51:12 +0530

    Add files via upload

---
+++ basicConceptsOfJava/OopEncapsulation.java
@@ -0,0 +1,41 @@
+package OopsConcepts;
+
+class Person {
+ 
+ // Encapsulating the name and age  only approachable and used using methods defined
+ private String name;
+ private int age;
+ private String address;
+
+ public String getName() { 
+	 return name; 
+	 }
+ public void setName(String name) { 
+	 this.name = name; 
+	 }
+ public int getAge() { 
+	 return age; 
+	 }
+ public void setAge(int age) { 
+	 this.age = age; 
+	 }
+ public String getAddress() { 
+	 return address; 
+	 }
+ public void setAdress(String address) { 
+	 this.address = address; 
+	 }
+}
+
+public class OopEncapsulation {
+ public static void main(String[] args)
+ {
+     Person hary = new Person();
+     hary.setName("Harry Potter");
+     hary.setAge(25);
+     hary.setAdress("5, Nehru street, Mumbai");
+     System.out.println("Name: " + hary.getName());
+     System.out.println("Age: " + hary.getAge());
+     System.out.println("Address: "+hary.getAddress());
+ }
+}
