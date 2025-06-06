97224abb588141e638d170a7130c06d436de1d77
Harini R R <141791926+HariniRR@users.noreply.github.com>
2025-06-06 15:25:09 +0530

    Add files via upload

---
+++ practice/TechGig_Practice/String/Numeric.java
@@ -0,0 +1,36 @@
+/*You just need to take string input and checks whether the string consists of only numeric characters.
+
+Input Format
+You will be given a function with string as single argument. 
+
+Constraints
+1 <= |S| <= 10^4
+
+Output Format
+You need to return the string value either "True" or "False". */
+
+import java.io.*;
+import java.util.*;
+public class Numeric {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.print(isNumeric(str));
+    	//Write code here
+
+   }
+   static String isNumeric(String input){
+       int i=input.len-1;
+       while(i>=0){
+           if(Character.isDigit(input.charAt(i))){
+               return "False";
+           }
+           else{
+               continue;
+           }
+           i--;
+       }
+       return "True";
+   }
+}
+
